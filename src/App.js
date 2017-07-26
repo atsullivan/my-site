c71a76a4953be6b18dfd7036301c1f571cb0d637
Andrew Sullivan <andrew.sullivan@procore.com>
2017-07-25 21:55:06 -0700

    Add gradient nabber and custom fonts

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
-import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Grid, Navbar, Nav, NavItem, Row, Col, Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
+      <div className="app-body">
+
+        <Navbar className="custom-navbar" fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">SULLY</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">About</NavItem>
+            <NavItem eventKey={2} href="#">Work</NavItem>
+            <NavItem eventKey={3} href="#">Contact</NavItem>
+          </Nav>
         </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
+
+        <Grid>
+          <Row className="intro">
+            <Col className="intro-col">
+              <h1>SULLY</h1>
+            </Col>
+          </Row>
+          <Row className="about">
+            <Col>
+              <p>
+                I'm a Developer with a passion for design, living in Santa Barbara, CA.
+                I work for Procore Technologies as a backend engineer.
+              </p>
+            </Col>
+          </Row>
+          <Row className="work">
+            <Col>
+            <h1>Latest Projects</h1>
+            </Col>
+          </Row>
+          <Row className="contact">
+            <Col>
+            <h1>Let's talk</h1>
+            <h2>andrew.sullivan@procore.com</h2>
+            </Col>
+          </Row>
+          <Row className="footer">
+            <Col>
+            <h1>Links</h1>
+            </Col>
+          </Row>
+        </Grid>
+
       </div>
     );
   }
