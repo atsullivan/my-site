4693f3c3909215f185413d4ea6fed077046499a6
Andrew Sullivan <andrew.sullivan@procore.com>
2017-07-26 21:31:32 -0700

    Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -8,29 +8,11 @@ class App extends Component {
     return (
       <div className="app-body">
 
-        {/* <Navbar className="custom-navbar" fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="/">SULLY</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-        </Navbar> */}
-{/* 
-        <div className="icon">
-          <h3 className='lg'>su</h3>
-          <h3 className='lg'>ll</h3>
-          <h3 className='lg'>y</h3>
-        </div> */}
-
         <div className="intro-col">
           <h2>Andrew Sullivan</h2>
           <h1>sully</h1>
           <h2>software engineer</h2>
         </div>
-
-
-
 
         <div className="footer">
           <i href="/" className="fa fa-linkedin-square fa-3x"></i>
