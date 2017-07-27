3dbef6f21b561f57369c01ee1cfc0d9ac731886f
Andrew Sullivan <andrew.sullivan@procore.com>
2017-07-26 22:01:09 -0700

    Start about section.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,38 @@ class App extends Component {
     return (
       <div className="app-body">
 
-        <div className="intro-col">
+        <div className="header">
           <h2>Andrew Sullivan</h2>
           <h1>sully</h1>
           <h2>software engineer</h2>
         </div>
 
+        {/* picture, paragraph about myself */}
+        <div className="about">
+          <h1>Hi, I'm Andrew Sullivan.</h1>
+          <p>
+            I'm a rails developer working at Procore Technologies in beautiful Santa Barbara California.
+            I am as passionate about write code as I am about designing beautiful products.
+          </p>
+        </div>
+
+
+
+        {/* web development, ui design, ... */}
+        <div className="whatido"></div>
+        {/* languages, tools, knowledge */}
+        <div className="skills"></div>
+        <div className="work"></div>
+
+
+
         <div className="footer">
-          <i href="/" className="fa fa-linkedin-square fa-3x"></i>
+          <i className="fa fa-envelope-o fa-3x"></i>
+          <i className="fa fa-linkedin-square fa-3x"></i>
           <i className="fa fa-github fa-3x"></i>
           <i className="fa fa-instagram fa-3x"></i>
           <i className="fa fa-facebook-official fa-3x"></i>
+          <h1>sully</h1>
         </div>
 
       </div>
